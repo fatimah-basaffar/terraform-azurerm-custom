d4afebed4058a4f0287df558a6f3cc2a2469dc53
fatimah-basaffar <fa.basaffar@gmail.com>
2025-04-28 10:51:09 +0300

    Updated

--- azurerm_application_gateway/application_gateway.tf
+++ azurerm_application_gateway/application_gateway.tf
@@ -1,4 +1,4 @@
-resource "azurerm_application_gateway" "network" {
+resource "azurerm_application_gateway" "app_gateway" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
---
+++ azurerm_application_gateway/output.tf
@@ -0,0 +1,5 @@
+output "app_gateway" {
+
+    value = azurerm_application_gateway.app_gateway
+  
+}
